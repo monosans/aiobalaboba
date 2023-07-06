c2ee8430813f231f3320064c14ea8c809d53aabb
monosans <hsyqixco@protonmail.com>
2023-07-06 18:38:12 +0300

    Remove useless pragmas

--- tests/test_aiobalaboba.py
+++ tests/test_aiobalaboba.py
@@ -7,9 +7,9 @@ from aiohttp import ClientSession
 
 from aiobalaboba import Balaboba
 
-if sys.version_info < (3, 8):  # pragma: <3.8 cover
+if sys.version_info < (3, 8):
     from typing_extensions import Literal
-else:  # pragma: >=3.8 cover
+else:
     from typing import Literal
 
 
